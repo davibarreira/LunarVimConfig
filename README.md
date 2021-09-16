af4aca26d92d940184e72426bb32be02ecee2c8c
Davi Barreira <davisbarreira@gmail.com>
2021-09-15 22:58:06 -0300

    LunarVim

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
 # LunarVimConfig
+
+This repo contains my configuration for
+[LunarVim](https://www.lunarvim.org/).
+The focus was to create an IDE for Julia, although
+it can be adapted to any other language by installing the appropriate plugins.
+
+[This blog post](https://medium.com/coffee-in-a-klein-bottle/vim-for-julia-another-look-1dc4265bb49b)
+is reposted here and presents a quick overview on how to use
+LunarVim and set it up for Julia.
+
+
+---
+
+![LunarVim](./figures/lunarvim.png)
+# Vim for Julia - Another Look
+## LunarVim as a Julia IDE
+In the past, I wrote on how to use Vim for Julia. Recently, I've changed my setup and I've been using the new and amazing LunarVim. Here is a brief tutorial on how to setup Vim (actually Neovim) as your Julia IDE.
+### Introducing LunarVim
+The first question to be answered is, what is LunarVim? Presto!
+
+LunarVim is an opinionated, extensible, and fast IDE layer for Neovim >= 0.5.0. LunarVim takes advantage of the latest Neovim features such as Treesitter and Language Server Protocol support.
+
+In simpler words, it's a series of default configurations for Neovim that makes it even more amazing. The first requirement to use LunarVim is to install Neovim with version at least 0.5. Unfortunately, the sudo apt install neovim will not work (at the time I'm writing this), because the version installed will be lower than the required one.
+
+An easy way to install a proper version is to add the PPA for the unstable version and install it. Here are the easy steps:
+```julia
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+sudo apt install neovim
+```
+To install LunarVim, just run:
+```
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+```
+By running the command lvim in your terminal, LunarVim should start! You can always add alias vim = "lvim" to your .bashrc , to run LunarVim instead of vim.
+
+### Setting up Julia
+
+Now that you've got LunarVim working, let's setup the Julia language. This is actually surprisingly simple. In your terminal, run the following:
+```
+julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
+```
+This installs the Language Server Protocol (LSP) for Julia, i.e. the auto-completion functionally for Julia. Now, every time you open a ".jl" file, just wait a bit and the LSP will start.
+
+Next, let's install the Julia-Vim package that will enable us using Unicode, thus, by writing something like \alpha and pressing tab, we'll get the alpha Unicode, which is allowed in Julia. To do this, go to your LunarVim configurations file, which can be accessed by running lvim in the terminal and selecting the Configuration option. Another way is to open the configuration file directly, which is ~/.config/lvim/config.lua .
+
+Inside the configuration, there is a place where you can easily install any plugin you want. Just navigate to where the "- - Additional Plugins" is. Originally, everything should be commented. Just uncomment the necessary lines, and write {"JuliaEditorSupport/julia-vim"} and save. This will prompt the installation of the plugging. Take a look at the figure below.
+
+
+![LunarVim](./figures/configlunar.png)
+
+This is what your configuration should look like to install Julia-Vim. Note that you can add any other plugins you like.
+
+**Word of Caution!**
+
+Since Vim is inside your terminal, you need your terminal to have a font with Unicode enabled. I suggest you install JuliaMono, a beautiful font created for Julia :D. Once the font is installed, just go into your terminal configuration and change to it.
+
+Even with Unicode font enabled, in my notebook, the Julia-Vim was still freezing after pressing Tab without any text. To solve this issue, I wrote the following two commands in my LunarVim configuration:
+
+```
+vim.cmd("let g:latex_to_unicode_tab = 'off'")
+vim.cmd("let g:latex_to_unicode_keymap = 1")
+```
+![LunarVim](./figures/configlunar2.png)
+
+Screenshot of my own configuration file.Now everything should be working beautifully!
+**Your new LunarVim IDE for Julia is ready to be used.**
+
+---
+
+###  Fast Course on LunarVim + Julia
+You can now read the documentation on LunarVim to better understand some of the default settings. But, I'll give some tips on how LunarVim works, and how to use it with Julia to run your code. Here is a (very) fast course on some of the main commands for LunarVim:
+
+![LunarVim](./figures/lunarvimcommand.png)
+
+* In LunarVim, your <leader> is the space , hence, many short cuts will be composed of pressing "space" followed by something else. Here is where LunarVim comes shining. If you just press "space" and wait a bit. A menu will pop-up from below, showing possible commands.
+
+![LunarVim](./figures/lunarmenu.png)
+
+* LunarVim comes with "NerdCommenter" plugin, which allows you to navigate with a menu. Just press <space>+e .
+
+![LunarVim](./figures/bufferlunar.png)
+
+* Once you open another file, a buffer is created and shown in the top of the screen . You can click on the tab to change buffers, or you can press shift+h or shift+l to change buffers.
+
+![LunarVim](./figures/julialunar.png)
+
+* Press ctrl+w to see the commands related to splitting screen and moving between screens. You can press ctrl+ h,l,j,k to move between windows.
+
+![LunarVim](./figures/highlight.png)
+
+* As in regular Vim, you can press / to start searching and then space+h clears the highlighted terms in the search.
+
+![LunarVim](./figures/tipjulia.png)
+
+* When you open a Julia file, you just need to wait a bit for the LSP to start working. Once the LSP is loaded, auto-completion will be working, and many other helpful features, such as visualizing the docstring of a function. For that, press g and a window will pop-up with many shortcuts related to the LSP. For example, you can press g + p to take a peek at the docstring and g + d to actually open the function definition in another buffer. Look the figures below.
+
+![LunarVim](./figures/repljulia.png)
+
+* Lastly, you can press ctrl+t to open and minimize a floating terminal. Once this is done, you can run the Julia REPL and copy/paste every line of code you want to run.
+
+There are many other helpful commands. Check-out the documentation on LunarVim or try them out by yourself to learn more. Hope this was helpful.
---
+++ config.lua
@@ -0,0 +1,171 @@
+-- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+vim.cmd("let g:latex_to_unicode_tab = 'off'")
+vim.cmd("let g:latex_to_unicode_keymap = 1")
+vim.opt.wrap = true -- display lines as one long line
+vim.opt.spelllang = "en" -- default spell check language
+lvim.keys.normal_mode["<A-S-e>"] = ":set spelllang=en<CR>"
+lvim.keys.visual_mode["<A-S-p>"] = ":set spelllang=pt_br<CR>"
+
+-- vim.cmd("let g:floaterm_keymap_new = '<C-j>'")
+
+-- Davi - Configurando Floating Terminal
+-- lvim.keys.normal_mode["<C-'>"] = ":FloatermNew!julia<CR>"
+vim.cmd("let g:floaterm_keymap_toggle = '<C-j>'")
+vim.cmd("let g:floaterm_position = 'right'")
+vim.cmd("let g:floaterm_wintype = 'float'")
+vim.cmd("let g:floaterm_height = 1.0")
+vim.cmd("let g:floaterm_width = 0.5")
+-- vim.cmd("let g:floaterm_shell = 'kitty'")
+lvim.keys.normal_mode["<C-b>"] = ":FloatermSend<CR><esc>j:FloatermToggle<CR>"
+lvim.keys.visual_mode["<C-b>"] = ":'<,'>FloatermSend<CR>:FloatermToggle<CR>"
+lvim.keys.normal_mode["<F9>"] = ":%FloatermSend<CR>:FloatermToggle<CR>"
+lvim.keys.normal_mode["<A-S-l>"] = ":FloatermNew<CR>"
+lvim.keys.normal_mode["<A-S-]>"] = ":FloatermNext<CR>"
+lvim.keys.normal_mode["<C-a>"] = ":FloatermSend!;clear<CR>:FloatermToggle<CR><BS>"
+
+-- general
+vim.opt.shiftwidth = 4
+lvim.format_on_save = true
+lvim.lint_on_save = true
+-- lvim.colorscheme = "onedarker"
+-- lvim.colorscheme = "onedark"
+lvim.colorscheme = "sonokai"
+vim.cmd("let g:sonokai_style = 'andromeda'")
+vim.cmd("let g:sonokai_enable_italic_comment = 1")
+
+lvim.lsp.diagnostics.virtual_text = false
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+-- unmap a default keymapping
+-- lvim.keys.normal_mode["<C-Up>"] = ""
+-- edit a default keymapping
+-- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+
+-- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
+-- lvim.builtin.telescope.on_config_done = function()
+--   local actions = require "telescope.actions"
+--   -- for input mode
+--   lvim.builtin.telescope.defaults.mappings.i["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-k>"] = actions.move_selection_previous
+--   lvim.builtin.telescope.defaults.mappings.i["<C-n>"] = actions.cycle_history_next
+--   lvim.builtin.telescope.defaults.mappings.i["<C-p>"] = actions.cycle_history_prev
+--   -- for normal mode
+--   lvim.builtin.telescope.defaults.mappings.n["<C-j>"] = actions.move_selection_next
+--   lvim.builtin.telescope.defaults.mappings.n["<C-k>"] = actions.move_selection_previous
+-- end
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+-- lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+-- }
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- generic LSP settings
+-- you can set a custom on_attach function that will be used for all the language servers
+-- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+-- you can overwrite the null_ls setup table (useful for setting the root_dir function)
+-- lvim.lsp.null_ls.setup = {
+--   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
+-- }
+-- or if you need something more advanced
+-- lvim.lsp.null_ls.setup.root_dir = function(fname)
+--   if vim.bo.filetype == "javascript" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
+--       or require("lspconfig/util").path.dirname(fname)
+--   elseif vim.bo.filetype == "php" then
+--     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
+--   else
+--     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
+--   end
+-- end
+
+-- set a formatter if you want to override the default lsp one (if it exists)
+-- lvim.lang.python.formatters = {
+--   {
+--     exe = "black",
+--     args = {}
+--   }
+-- }
+-- set an additional linter
+-- lvim.lang.python.linters = {
+--   {
+--     exe = "flake8",
+--     args = {}
+--   }
+-- }
+
+-- Additional Plugins
+lvim.plugins = {
+    {"folke/tokyonight.nvim"},
+    -- {
+    --     "ray-x/lsp_signature.nvim",
+    --     config = function() require"lsp_signature".on_attach() end,
+    --     event = "InsertEnter"
+    -- },
+    {"KeitaNakamura/neodark.vim"},
+    {"rakr/vim-one"},
+    {"joshdick/onedark.vim"},
+    {"lervag/vimtex"},
+    {"JuliaEditorSupport/julia-vim"},
+    {"projekt0n/github-nvim-theme"},
+    {
+      "iamcco/markdown-preview.nvim",
+      run = "cd app && npm install",
+      ft = "markdown",
+      config = function()
+        vim.g.mkdp_auto_start = 0
+      end,
+    },
+    {
+      "norcalli/nvim-colorizer.lua",
+        config = function()
+          require("colorizer").setup({ "*" }, {
+              RGB = true, -- #RGB hex codes
+              RRGGBB = true, -- #RRGGBB hex codes
+              RRGGBBAA = true, -- #RRGGBBAA hex codes
+              rgb_fn = true, -- CSS rgb() and rgba() functions
+              hsl_fn = true, -- CSS hsl() and hsla() functions
+              css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+              css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+              })
+      end,
+    },
+    {
+      "rktjmp/lush.nvim",
+    },
+    {"rafi/awesome-vim-colorschemes"},
+    {"voldikss/vim-floaterm"},
+}
+
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- lvim.autocommands.custom_groups = {
+--   { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+-- }
